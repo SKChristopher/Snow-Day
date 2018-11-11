89e0df63b3872b3a67d0fa2c416e7a88f8aea594
Christopher Washburn <chris132128@gmail.com>
2018-11-10 22:18:48 -0800

    initialized

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude:  /node_modules/,
+        use: "babel-loader",
+      }
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
