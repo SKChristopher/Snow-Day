cbd58221905f325f93cf8c54fada1f5f8dc4f7ae
Christopher <chris132128@gmail.com>
2018-12-18 01:29:55 -0800

    constantly generate random teachers; add loseLife method

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,23 @@ class App extends Component {
 
   componentDidMount = () => {
     document.addEventListener('keydown', this.handleKeyDown);
+
+    // send endless wave of teachers
+    const createTeacher = () => {
+      // TODO: update this makeid function
+      function makeid() {
+        let text = '';
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        for (let i = 0; i < 5; i += 1) text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+      }
+      const { teachersOnField } = this.state;
+      const randomTeacher = makeid(); // TODO: generate random teacher names
+      teachersOnField[randomTeacher] = 1;
+      this.setState({ teachersOnField });
+    };
+
+    setInterval(createTeacher, 2500);
   }
 
   handleKeyDown = (event) => {
@@ -39,15 +56,17 @@ class App extends Component {
   }
 
   checkForDefeatedTeachers = () => {
-    const { userInput, teachersOnField } = this.state;
+    const { teachersOnField } = this.state;
+    let { userInput } = this.state;
     Object.keys(teachersOnField).forEach((teacher) => {
       if (userInput.includes(teacher) && teachersOnField[teacher] !== 'defeated') {
         // defeat teacher and add to score
         this.updateScore(teacher.length);
         teachersOnField[teacher] = 'defeated';
+        userInput = '';
       }
     });
-    this.setState({ teachersOnField });
+    this.setState({ teachersOnField, userInput });
   }
 
   updateScore = (num) => {
@@ -56,14 +75,27 @@ class App extends Component {
     this.setState({ score });
   }
 
+  loseLife = (name) => {
+    const { teachersOnField } = this.state;
+    let { lives } = this.state;
+    if (teachersOnField[name] !== 'defeated') {
+      teachersOnField[name] = 'defeated';
+      lives -= 1;
+      console.log(name);
+    }
+    // TODO: add defeat logic here at 0 lives
+    this.setState({ lives, teachersOnField });
+  }
+
   render() {
     const {
       level, lives, score, teachersOnField,
     } = this.state;
+    const { loseLife } = this;
     return (
       <div>
         <Databar level={level} lives={lives} score={score} />
-        <Battlefield level={level} teachersOnField={teachersOnField} />
+        <Battlefield level={level} teachersOnField={teachersOnField} loseLife={loseLife} />
       </div>
     );
   }
